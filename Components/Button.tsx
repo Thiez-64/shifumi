dd822c963af6d7d12448d23f03e1077b963bb731
Thiez-64 <ma.ojeanson@gmail.com>
2021-05-11 15:25:28 +0200

    First test

--- Components/Button.tsx
+++ Components/Button.tsx
@@ -1,11 +1,13 @@
+import styles from "../styles/Home.module.css";
+
 interface Iprops {
   input: string;
 }
 
 function Button({ input }: Iprops) {
   return (
-    <button className="w-40 h-40 bg-red">
-      <div>{input}</div>
+    <button className="w-60 h-40 bg-red-600">
+      <div className="text-white font-bold">{input}</div>
     </button>
   );
 }
