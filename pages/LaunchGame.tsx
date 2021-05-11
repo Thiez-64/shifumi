6c42ea67801d107e5d98d969757fd0ee9bb612ef
Thiez-64 <ma.ojeanson@gmail.com>
2021-05-11 22:02:05 +0200

    LaunchGame page has been created

--- pages/LaunchGame.tsx
+++ pages/LaunchGame.tsx
@@ -1,13 +1,11 @@
 import Image from "next/image";
 import Button from "../Components/Button";
-import MyInput from "../Components/MyInput";
 import Users from "../Components/Users";
 
-function TwoPlayer(): JSX.Element {
-  const create = { input: "CREATE" };
-  const play = { input: "LET'S PLAY" };
+function LaunchGame(): JSX.Element {
+  const go = { input: "PLAY" };
   const playerOne = { name: "Jean-Neymar" };
-
+  const playerTwo = { name: "Louis-Vianey" };
   return (
     <div className="w-full h-full bg-gray-400">
       <header className="flex items-center justify-center h-40 py-2">
@@ -24,22 +22,21 @@ function TwoPlayer(): JSX.Element {
       </header>
       <div className="m-8">
         <h1 className="flex justify-center m-4 text-2xl font-bold text-center">
-          Create a game
+          Please wait a second Player
         </h1>
-        <MyInput />
-        <Button {...create} />
+        <Users {...playerOne} />
       </div>
-      <p className="justify-center m-8 text-center">--</p>
+      <h1 className="justify-center m-8 text-center">VS</h1>
       <h1 className="flex justify-center m-4 text-2xl font-bold text-center">
         Player
       </h1>
       <div className="m-8">
-        <Users {...playerOne} />
-        <Button {...play} />
+        <Users {...playerTwo} />
+        <Button {...go} />
       </div>
       <footer className="flex items-center justify-center h-40 py-2"></footer>
     </div>
   );
 }
 
-export default TwoPlayer;
+export default LaunchGame;
