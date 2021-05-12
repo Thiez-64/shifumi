a1eace953f5d7cc11e2b34b894057a53560e22d4
MatthiasWanner <matthias.wanner@gmail.com>
2021-05-12 07:47:00 +0200

    repair links in index

--- Components/Button.tsx
+++ Components/Button.tsx
@@ -1,32 +1,16 @@
-import React, { useState } from "react";
-import { useRouter } from "next/router";
-
 interface Iprops {
   input: string;
-  href?: string;
   onClick?: () => void;
 }
 
-function Button({ input, href, onClick }: Iprops): JSX.Element {
-  const router = useRouter();
-
-  const [IsClicked, setIsClicked] = useState(false);
-
+function Button({ input, onClick }: Iprops): JSX.Element {
   return (
     <div className="flex justify-center">
       <button
         onClick={onClick}
-        className={`h-10 ${
-          IsClicked ? "bg-white border-red-600" : "bg-red-600 border-white"
-        } rounded-3xl focus:outline-none`}
+        className={`h-10 bg-red-600 border-white rounded-3xl focus:outline-none`}
       >
-        <div
-          className={`${
-            IsClicked ? "text-red-600" : "text-white"
-          } font-bold px-4`}
-        >
-          {input}
-        </div>
+        <div className={`text-white font-bold px-4`}>{input}</div>
       </button>
     </div>
   );
--- pages/HomeStart.tsx
+++ pages/HomeStart.tsx
@@ -1,9 +1,11 @@
 import Button from "../Components/Button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function HomeStart(): JSX.Element {
-  const start = { input: "START", href: "/OnePlayer" };
+  const start = { input: "START" };
   const multi = { input: "MULTI-PLAYER", href: "/Register" };
+
   return (
     <div className="flex flex-col items-center h-screen bg-gray-400 bg-center bg-no-repeat bg-cover">
       <div className="px-16 text-center infos md:text-center font-Quantico w-50">
@@ -26,11 +28,19 @@ export default function HomeStart(): JSX.Element {
       </div>
 
       <div className=" pt-9 ">
-        <Button {...start} />
+        <Link href="/OnePlayer">
+          <a>
+            <Button {...start} />
+          </a>
+        </Link>
       </div>
 
       <div className="pt-6">
-        <Button {...multi} />
+        <Link href="/Register">
+          <a>
+            <Button {...multi} />
+          </a>
+        </Link>
       </div>
     </div>
   );
